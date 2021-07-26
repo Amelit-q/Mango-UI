f49c3e00dccb9387549d37df2bfc66efc78ca85e
Amelit <muradovt111@gmail.com>
2021-07-26 22:48:08 +0300

    added fields for response: getters and setters

--- src/stores/AuthStore.ts
+++ src/stores/AuthStore.ts
@@ -1,10 +1,18 @@
-import {makeAutoObservable, observable} from "mobx"
+import {action, makeAutoObservable, observable} from "mobx"
 import {AuthApi} from "../api/AuthApi"
 import {RegisterCommand} from "../types/Auth/Requests/RegisterCommand"
 
 export class AuthStore {
-
     private registrationApi = new AuthApi()
+
+    @observable
+    private _message: String | null = null
+
+    @observable
+    private _success: Boolean = false
+
+    @observable
+    private _userId: String | null = null
 
     @observable
     private _login: String | null = null
@@ -12,10 +20,28 @@ export class AuthStore {
     @observable
     private _isLogged: boolean = false
 
+    @action
+    public setMessage = (message: string) => {
+        this._message = message
+    }
+
+    @action
+    public setSuccess = (success: boolean) => {
+        this._success = success
+    }
+
+    @action
+    public setUserId = (userId: string) => {
+        this._userId = userId
+    }
 
     public async registration(data: RegisterCommand) {
         try {
+            //@ts-ignore
             const {message, success, userId} = await this.registrationApi.register(data)
+            this.setMessage(message)
+            this.setSuccess(success)
+            this.setUserId(userId)
             return Promise.resolve()
         } catch (error) {
             return Promise.reject(error)
@@ -24,8 +50,5 @@ export class AuthStore {
 
     public constructor() {
         makeAutoObservable(this)
-
     }
-
-
 }
