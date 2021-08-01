ba2b93958f64870d8a0dc3142068fd3cc773472e
Amelit <muradovt111@gmail.com>
2021-08-01 21:00:16 +0300

    added the rest fields for session

--- src/stores/entities/SessionEntity.ts
+++ src/stores/entities/SessionEntity.ts
@@ -1,6 +1,6 @@
-import {observable} from "mobx"
+import {action, computed, observable} from "mobx"
 
-export interface DefaultSession {
+export interface IDefaultSession {
     userId: string | null
     refreshToken: string | null
     accessToken: string | null
@@ -15,6 +15,44 @@ export class DefaultSessionEntity {
 
     @observable
     protected _accessToken: string | null = null
+
+
+    @action
+    private setAccessToken = (value: string | null) => {
+        this._accessToken = value
+    }
+
+    @action
+    private setRefreshToken = (value: string | null) => {
+        this._refreshToken = value
+    }
+
+    @action
+    private setUserId = (value: string | null) => {
+        this._userId = value
+    }
+
+
+    public constructor(session: IDefaultSession) {
+        this.setAccessToken(session.accessToken)
+        this.setRefreshToken(session.refreshToken)
+        this.setUserId(session.userId)
+    }
+
+    @computed
+    public get accessToken() {
+        return this._accessToken
+    }
+
+    @computed
+    public get refreshToken() {
+        return this._refreshToken
+    }
+
+    @computed
+    public get userId() {
+        return this._userId
+    }
 
 
 }
