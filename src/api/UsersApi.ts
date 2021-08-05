671c91b443a238de7dd2192cebc6c9378773ac93
Amelit <muradovt111@gmail.com>
2021-08-05 22:43:53 +0300

    created new Store plus api class

--- src/api/UsersApi.ts
+++ src/api/UsersApi.ts
@@ -1,4 +1,24 @@
-﻿export class UsersApi {
+﻿import axios, {AxiosInstance} from "axios"
+import {ApiRoute} from "../consts/Routes"
+
+export class UsersApi {
+
+    protected headers: {
+        "Content-Type": "application/json"
+    } = {
+        "Content-Type": "application/json",
+    }
+
+    protected apiConnector: AxiosInstance
+
+    public constructor() {
+        this.apiConnector = axios.create({headers: this.headers, baseURL: ApiRoute.apiDomain})
+    }
+
+
+
+
+
     public async getUser(data: any) {
     }
 }
---
+++ src/stores/UsersStore.ts
@@ -0,0 +1,4 @@
+export class UsersStore {
+
+
+}
