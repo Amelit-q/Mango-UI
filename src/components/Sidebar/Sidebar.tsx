77201fc0d9a002df32271cd2fbad4ff4a2b5098f
Daniil-Perevezentsev <Rondo4r@gmail.com>
2021-10-16 21:14:39 +0300

    recreate Navbar and sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -11,250 +11,169 @@ import {ReactComponent as Searchsvg} from "../../assets/imgNavbar/Searchicon.svg
 
 export const Sidebar = () => {
     return (
-        <NavBarContainer>
-            <AContainer>
-                <APicture>
-                </APicture>
-            </AContainer>
-            <LinkContainer>
-                <HomeContainer>
-                    <HomePicture>
-                        <Homesvg />
-                    </HomePicture>
-                </HomeContainer>
-                <ChatContainer>
-                    <ChatPicture>
-                        <Chatsvg />
-                    </ChatPicture>
-                </ChatContainer>
-                <CallContainer>
-                    <CallPicture>
-                        <Callsvg />
-                    </CallPicture>
-                </CallContainer>
-                <ContactContainer>
-                    <ContactPicture>
-                        <Contactsvg />
-                    </ContactPicture>
-                </ContactContainer>
-                <OptionContainer>
-                    <OptionPicture>
-                        <Optionsvg />
-                    </OptionPicture>
-                </OptionContainer>
-                <SerachContainer>
-                    <SearchPicture>
-                        <Searchsvg />
-                    </SearchPicture>
-                </SerachContainer>
-            </LinkContainer>
-            <Line></Line>
-        </NavBarContainer>
+        <Container>
+            <Logo>
+                <Homesvg>
+
+                </Homesvg>
+            </Logo>
+            <BtnBox>
+                <Home>
+                    <Homesvg>
+
+                    </Homesvg>
+                </Home>
+                <Chat>
+                    <Chatsvg>
+
+                    </Chatsvg>
+                </Chat>
+                <Call>
+                    <Callsvg>
+
+                    </Callsvg>
+                </Call>
+                <Contact>
+                    <Contactsvg>
+
+                    </Contactsvg>
+                </Contact>
+                <Search>
+                    <Searchsvg>
+
+                    </Searchsvg>
+                </Search>
+                <Opon>
+                    <Optionsvg>
+
+                    </Optionsvg>
+                </Opon>
+                <Footer>
+
+                </Footer>
+            </BtnBox>
+        </Container>
     )
+
 }
 
-
-const SearchPicture = styled("div")`
-  position: absolute;
-  height: 22px;
-  left: 36.9%;
-  right: 36.9%;
-  top: calc(50% - 22px / 2);
-  font-family: Material Icons;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 22px;
-  line-height: 22px;
-  /* identical to box height */
-
-  text-align: center;
-
-  color: #6B59CC;
-
-`
-const SerachContainer = styled("div")`
-  position: absolute;
-  height: 72px;
-  left: 0%;
-  right: 0%;
-  top: 313px;
-
-`
-
-const Line = styled("div")`
-  display: inline-block;
-  position: absolute;
-  left: 83px;
-  height: 100%;
-  border: 1px solid #ECEEF5;
-  box-sizing: border-box;
-
-`
-
-const APicture = styled("div")`
-  position: absolute;
-  left: 0%;
-  right: 0%;
-  top: 0%;
-  bottom: 0%;
-
-  background: #28b463;
-  border-radius: 14px;
-`
-
-const AContainer = styled("div")`
-  position: absolute;
-  width: 40px;
-  height: 40px;
-  left: 22px;
-  top: 22px;
-`
-
-const OptionContainer = styled("div")`
-  position: absolute;
-  height: 72px;
-  left: 0%;
-  right: 0%;
-  top: 385px;
-`
-
-const OptionPicture = styled("div")`
-  position: absolute;
-  height: 22px;
-  left: 36.9%;
-  right: 36.9%;
-  top: calc(50% - 22px / 2);
-
-  font-family: Material Icons;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 22px;
-  line-height: 22px;
-  /* identical to box height */
-
-  text-align: center;
-
-  color: #8083A3;
-`
-
-const ContactPicture = styled("div")`
-  position: absolute;
-  width: 22px;
-  height: 22px;
-  left: calc(50% - 22px / 2);
-  top: calc(50% - 22px / 2);
-
-  font-family: Material Icons;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 22px;
-  line-height: 22px;
-  /* identical to box height */
-
-  text-align: center;
-
-  color: #28b463;
-
-`
-
-const ContactContainer = styled("div")`
-  position: absolute;
-  height: 72px;
-  left: 0%;
-  right: 0%;
-  top: 241px;
-
-`
-
-const CallPicture = styled("div")`
-  position: absolute;
-  height: 22px;
-  left: 36.9%;
-  right: 36.9%;
-  top: calc(50% - 22px / 2);
-
-  font-family: Material Icons;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 22px;
-  line-height: 22px;
-  /* identical to box height */
-
-  text-align: center;
-
-  color: #8083A3;
-`
-
-const CallContainer = styled("div")`
-  position: absolute;
-  height: 72px;
-  left: 0%;
-  right: 0%;
-  top: 169px;
-`
-
-const ChatPicture = styled("div")`
-  position: absolute;
-  height: 22px;
-  left: 36.9%;
-  right: 36.9%;
-  top: calc(50% - 22px / 2);
-
-  font-family: Material Icons;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 22px;
-  line-height: 22px;
-  /* identical to box height */
-
-  text-align: center;
-
-  color: #8083A3;
-`
-
-const ChatContainer = styled("div")`
-  position: absolute;
-  height: 72px;
-  left: 0%;
-  right: 0%;
-  top: 97px;
-
-`
-
-const NavBarContainer = styled("nav")`
+const Logo = styled("div")`
   display: flex;
   flex-direction: column;
-  background: #FFFFFF;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 15px 20px 25px 25px;
 
-`
-
-const LinkContainer = styled("div")`
-  position: absolute;
-  width: 84px;
-  height: 576px;
-  left: 0px;
-  top: 181px;
-`
-
-const HomePicture = styled("div")`
-
-  font-family: Material Icons;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 22px;
-  line-height: 22px;
-  /* identical to box height */
-
-  text-align: center;
-
-  color: #8083A3;
-`
-
-const HomeContainer = styled("div")`
-
-  position: relative;
-  height: 72px;
+  position: static;
+  height: 50px;
   left: 0%;
   right: 0%;
-  top: 25px;
+  top: 0px;
+`
+const Footer = styled("div")`
+  min-height: calc(20vh - 80px);
+`
+const Opon = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 15px 20px 25px 25px;
+
+  position: static;
+  height: 50px;
+  left: 0%;
+  right: 0%;
+  top: 672px;
+`
+const Search = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 15px 20px 25px 25px;
+
+  position: static;
+  height: 50px;
+  left: 0%;
+  right: 0%;
+  top: 384px;
+`
+const Contact = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 15px 20px 25px 25px;
+
+  position: static;
+  height: 50px;
+  left: 0%;
+  right: 0%;
+  top: 288px;
+`
+const Call = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 15px 20px 25px 25px;
+
+  position: static;
+  height: 50px;
+  left: 0%;
+  right: 0%;
+  top: 192px;
+
+`
+const Chat = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 15px 20px 25px 25px;
+
+  position: static;
+  height: 50px;
+  left: 0%;
+  right: 0%;
+  top: 96px;
+`
+const Home = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+  padding: 15px 20px 25px 25px;
+
+  position: static;
+  height: 50px;
+  left: 0%;
+  right: 0%;
+  top: 0px;
+`
+const BtnBox = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px;
+  margin: 110px 0px 0px 0px;
+  position: static;
+  height: 150%;
+`
+const Container = styled("div")`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5px 0px 0px;
+
+  position: static;
+  width: 84px;
+  height: 100%;
+  left: 0px;
+  top: 0px;
+
+  background: #FFFFFF;
 `
