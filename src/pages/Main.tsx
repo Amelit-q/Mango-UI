6a89fb9db339de110b0ee857c1aec256380380d9
Amelit <muradovt111@gmail.com>
2021-10-17 21:35:12 +0300

    fix for pipeline

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,4 @@
 import * as React from "react"
-import {UserRoutes} from "../consts/Routes"
-import {Navbar} from "../components/ProfileBar/ProfileBar"
 import styled from "styled-components"
 
 export const Main: React.FunctionComponent = () => {
