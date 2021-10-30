f98023415cd84398e7d6c39bdae829b41ebbfaf4
Amelit <muradovt111@gmail.com>
2021-10-30 18:54:32 +0300

    unused files was deleted

--- src/components/ToolbarButton/ToolbarButton.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./ToolbarButton.css";
-
-// @ts-ignore
-export const ToolbarButton = ({ icon }) => {
-  
-  return <i className={`toolbar-button ${icon}`} />;
-};
