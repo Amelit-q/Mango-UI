7f0c7d80728307cd58819d64bd7e321353c633bf
Amelit <muradovt111@gmail.com>
2021-10-30 18:59:01 +0300

    update app file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import {BrowserRouter, Route, Switch} from "react-router-dom"
-import {Messenger} from "../Messenger/Messenger"
 import {Registration} from "../../pages/Registration"
 import {Main} from "../../pages/Main"
 import {Verification} from "../../pages/Verification"
@@ -12,7 +11,6 @@ export default function App() {
         <div className="App">
             <BrowserRouter>
                 <Switch>
-                    <Route exact path="/" component={Messenger} />
                     <Route exact path="/main" component={Main} />
                     <Route exact path="/registration" component={Registration} />
                     <Route exact path="/verification" component={Verification} />
