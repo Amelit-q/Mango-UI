0ab0e779e22ba3442bcfd009b15da21d3ce4d223
Amelit <muradovt111@gmail.com>
2021-10-31 18:04:58 +0300

    changed default route plus unused package was deleted

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,25 +1,21 @@
-import React from "react"
+import * as React from "react"
 import {BrowserRouter, Route, Switch} from "react-router-dom"
 import {Registration} from "../../pages/Registration"
 import {Main} from "../../pages/Main"
 import {Verification} from "../../pages/Verification"
 import {LoginForm} from "../LoginForm/LoginForm"
 
-
 export default function App() {
     return (
         <div className="App">
             <BrowserRouter>
                 <Switch>
-                    <Route exact path="/main" component={Main} />
+                    <Route exact path="/" component={Main} />
                     <Route exact path="/registration" component={Registration} />
                     <Route exact path="/verification" component={Verification} />
-                    {/*TODO: DELETE LOGIN TEST COMP TASK */}
                     <Route exact path="/login-test" component={LoginForm} />
-
                 </Switch>
             </BrowserRouter>
-
         </div>
     )
 }
